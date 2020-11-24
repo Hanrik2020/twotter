c6378e0e3de7e705dd3df9f96f49c8d2307433d9
Henrik Rydstrom <henrik.rydstrom@gmail.com>
2020-11-24 20:33:20 +0100

    next step, conditionals
    twotter

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,17 @@
 <template>
   <div id="app">
-    @{{ user.username }} - {{ fullName }}
-    <strong>Followers: {{ followers }}</strong>
-    <button v-on:click="followUser">Follow</button>
+    <UserProfile/>
   </div>
 </template>
   
 
 <script>
+import UserProfile from "./assets/components/UserProfile";
+
 export default {
   name: "App",
-  data() {
-    return {
-      followers: 0,
-      user: {
-        id: 1,
-        username: "hankenbrune",
-        firstName: "Henrik",
-        lastName: "Rydstrom",
-        email: "henrik.rydstrom@gmail.com",
-        isAdmin: true,
-      },
-    };
-  },
-  watch: {
-    followers(newFollowerCount, oldFollowerCount) {
-      if (oldFollowerCount < newFollowerCount) {
-        console.log(`${this.user.username} just gained a new follower!`);
-      }
-    },
-  },
-  computed: {
-    fullName() {
-      return `${this.user.firstName} ${this.user.lastName}`;
-    },
-  },
-  methods: {
-    followUser() {
-      this.followers++;
-    },
-  },
-  mounted() {
-    this.followUser();
-  },
-};
+  components: { UserProfile }
+  }
 </script>
 
 <style>
@@ -52,9 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   margin-top: 30px;
-  text-align: center;
-  color: #2c3e50;
-  display: flex;
-  flex-direction: column;
+  
+  
 }
 </style>
