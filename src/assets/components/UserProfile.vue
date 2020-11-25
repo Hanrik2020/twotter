30e80dea7de947d491a8001fa25c3df4f5317f75
Hanrik2020 <henrik.rydstrom@gmail.com>
2020-11-25 16:17:40 +0100

    created twooteritem component
    start at chapter 5

--- src/assets/components/UserProfile.vue
+++ src/assets/components/UserProfile.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="user-profile">
-  <div class="user-profile__user-panel">
-      <h1 class="user-profile__username"> @{{ user.username }} </h1>
-        <div class="user-prifle__follower-count">
-            <strong>Followers: {{ followers }}</strong>
-        </div>
-        
-  
+    <div class="user-profile__user-panel">
+      <h1 class="user-profile__username">@{{ user.username }}</h1>
+      <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+
+      <div class="user-prifle__follower-count">
+        <strong>Followers: {{ followers }}</strong>
       </div>
+    </div>
+    <div class="user-profile_twoots-wrapper">
+      <TwootItem
+        v-for="twoot in user.twoot"
+        :key="twoot.id"
+        :username="user.username"
+        :twoot="twoot"
+        @favorite="toggleFavorite"
+      />
+    </div>
   </div>
 </template>
   
 
 <script>
+import TwootItem from "./TwootItem";
+
 export default {
   name: "UserProfile",
+  components: { TwootItem },
   data() {
     return {
       followers: 0,
@@ -25,6 +37,10 @@ export default {
         lastName: "Rydstrom",
         email: "henrik.rydstrom@gmail.com",
         isAdmin: true,
+        twoot: [
+          { id: 1, content: "Hej hej halloe" },
+          { id: 2, content: "Don't eat humans." },
+        ],
       },
     };
   },
@@ -44,6 +60,9 @@ export default {
     followUser() {
       this.followers++;
     },
+    toggleFavorite(id) {
+      console.log(`Favorited twoot #${id}`);
+    },
   },
   mounted() {
     this.followUser();
@@ -53,25 +72,32 @@ export default {
 
 <style>
 .user-profile {
-    display: grid;
-    grid-template-columns: 1fr 3fr;
-    width: 100%;
-    padding: 50px 5%;
-    }
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  width: 100%;
+  padding: 50px 5%;
+}
 
 .user-profile__user-panel {
-    display: flex;
-    flex-direction: column;
-    margin-right: 50px;
-    padding: 20px;
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid #DFE3E8;
-    }
+  display: flex;
+  flex-direction: column;
+  margin-right: 50px;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #DFE3E8;
+}
+
+.user-profile__admin-badge {
+  background-color: rebeccapurple;
+  border-radius: 5px;
+  padding: 3px 5px;
+  color: white;
+  margin-right: auto;
+  font-weight: 600;
+}
 
 h1 {
-    margin: 0;
- }
-
-
+  margin: 0;
+}
 </style>
